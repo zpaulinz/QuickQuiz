4a761a6b53f487e8e421dfdf74134e458f3d8927
zpaulinz <zpaulinz@gmail.com>
2025-03-06 21:42:40 +0100

    Add styles for StartView and update quickquiz.py to include start view

--- src/quickquiz.py
+++ src/quickquiz.py
@@ -2,6 +2,7 @@ import json
 import random
 from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QStackedWidget
+from startview import StartView
 from questionview import QuestionView
 from resultsview import ResultsView
 
@@ -26,23 +27,32 @@ class QuickQuiz(QWidget):
         self.stacked_widget = QStackedWidget(self)
         self.layout = QVBoxLayout(self)
 
-        # Create question and results views
+        # Create views
+        self.create_start_view()
         self.create_question_view()
         self.create_results_view()
 
         self.layout.addWidget(self.stacked_widget)
         self.setLayout(self.layout)
+        
+        
+    # Initialize the start view
+    def create_start_view(self):   
+        self.start_view = StartView(self.start_quiz)
+        self.stacked_widget.addWidget(self.start_view)
 
+
+    # Initialize the question view
     def create_question_view(self):
-        # Initialize the question view and add it to the stacked widget
         self.question_view = QuestionView(self.questions[self.current_question], self.check_answer)
         self.stacked_widget.addWidget(self.question_view)
         
-
+        
+    # Initialize the results view
     def create_results_view(self):
-        # Initialize the results view and add it to the stacked widget
         self.results_view = ResultsView(self.score, self.play_again)
         self.stacked_widget.addWidget(self.results_view)
+
 
     def check_answer(self, selected_answer):
         # Check if the selected answer is correct
@@ -61,9 +71,11 @@ class QuickQuiz(QWidget):
             # Set a timer to delay showing results
             QTimer.singleShot(1200, self.show_results)  # 1200 ms = 1.2 s
             
+            
     def update_progress_bar(self, progress):
         # Updates the progress bar with the given progress percentage
         self.question_view.progress_bar.setValue(progress)
+        
         
     def update_question(self):
         # Calculate the progress
@@ -75,12 +87,14 @@ class QuickQuiz(QWidget):
         
         self.question_view.update(question_data, progress)
 
+
     def show_results(self):
         # Update the results view with the final score
         self.results_view.show_results(self.score, len(self.questions))
         
         # Switch to the results view
         self.stacked_widget.setCurrentWidget(self.results_view)
+
 
     def play_again(self):
         # Reset the quiz
@@ -90,7 +104,12 @@ class QuickQuiz(QWidget):
         random.shuffle(self.questions)
         
         self.update_question()
+        self.stacked_widget.setCurrentWidget(self.start_view)
+    
+    
+    def start_quiz(self):
         self.stacked_widget.setCurrentWidget(self.question_view)
+        
         
     def load_styles(self):
         # Load CSS styles from a file
