a2b5a11b79921770ba4fb4ef624e71b30b376f57
zpaulinz <zpaulinz@gmail.com>
2025-03-06 21:39:38 +0100

    Create startview.py

---
+++ src/startview.py
@@ -0,0 +1,30 @@
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtCore import Qt
+
+class StartView(QWidget):
+    def __init__(self, start_game_callback):
+        super().__init__()
+
+        self.layout = QVBoxLayout(self)
+    
+        self.title_label = QLabel(self)
+        self.title_label.setObjectName("titleLabel")  
+        self.title_label.setAlignment(Qt.AlignCenter) 
+        self.title_label.setText(f"QuickQuiz") 
+        
+        self.title_label.setFixedHeight(80)
+        self.layout.addWidget(self.title_label)
+
+        self.header_label = QLabel(self)
+        self.header_label.setObjectName("headerLabel")  
+        self.header_label.setAlignment(Qt.AlignCenter)  
+        self.header_label.setText(f"Sprawdź swoją wiedzę!") 
+
+        self.header_label.setFixedHeight(60)
+        self.layout.addWidget(self.header_label)
+        
+        self.start_button = QPushButton("Rozpocznij quiz", self)
+        self.start_button.setObjectName("startButton")  
+        self.start_button.clicked.connect(start_game_callback)
+        self.header_label.setFixedHeight(82)
+        self.layout.addWidget(self.start_button)
