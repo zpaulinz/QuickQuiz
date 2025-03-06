2102c446271251dee196cf5801fb940b5ab3edf7
zpaulinz <zpaulinz@gmail.com>
2025-03-06 22:48:01 +0100

    Update design and layout of resultsview

--- src/resultsview.py
+++ src/resultsview.py
@@ -11,6 +11,12 @@ class ResultsView(QWidget):
         self.result_label.setObjectName("resultLabel")  
         self.result_label.setAlignment(Qt.AlignCenter)  
         self.layout.addWidget(self.result_label)
+        
+        self.description_label = QLabel(self)
+        self.description_label.setObjectName("descriptionLabel")  
+        self.description_label.setAlignment(Qt.AlignCenter)  
+        self.description_label.setWordWrap(True)
+        self.layout.addWidget(self.description_label)
 
         self.play_again_button = QPushButton("Zagraj jeszcze raz", self)
         self.play_again_button.setObjectName("playAgainButton")  
@@ -20,3 +26,10 @@ class ResultsView(QWidget):
 
     def show_results(self, score, total_questions):
         self.result_label.setText(f"Twój wynik: {score} / {total_questions}")
+        
+        percent_score = round(score / total_questions * 100)
+        if score == total_questions:
+            self.description_label.setText(f"Gratulacje! Znasz wszystkie odpowiedzi!")
+        else:
+            self.description_label.setText(f"{percent_score}% poprawnych odpowiedzi")
+        
