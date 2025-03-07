926683559e9f8380c0c1a16f939cf66968539c8e
zpaulinz <zpaulinz@gmail.com>
2025-03-07 18:28:48 +0100

    Add punctuation mark

--- src/resultsview.py
+++ src/resultsview.py
@@ -36,5 +36,5 @@ class ResultsView(QWidget):
         if score == total_questions:
             self.description_label.setText(f"Gratulacje! Znasz wszystkie odpowiedzi!")
         else:
-            self.description_label.setText(f"Uzyskano {percent_score}% poprawnych odpowiedzi")
+            self.description_label.setText(f"Uzyskano {percent_score}% poprawnych odpowiedzi.")
         
